fd8cc2c83891af04cbf037870685c5dafa6bad60
Chirag <chirag.parsania@gmail.com>
2020-04-24 16:12:20 +0530

    initial commit

--- R/chip_functions.R
+++ R/chip_functions.R
@@ -3,28 +3,42 @@
 #' @param bw_file a string denoting path of .bw file
 #' @param gff_file a string denoting path of gff file.
 #'
-#' @return an object of class \link{GenomicRanges} which can be exported various file formats such as bw, bed, begraph etc.
+#' @return an object of class \link{GenomicRanges} which can be exported to various file formats such as bw, bed, begraph etc.
 #' @export
 #'
 #' @examples
-#'
-#'
-#'
+#' library("magrittr")
+#' bw_file <- system.file("extdata" ,
+#' "example.bw" ,
+#' package = "parcutils")
+#' gff_file <- system.file("extdata" ,
+#'                        "C_glabrata_CBS138_version_s02-m07-r06_features.gff" ,
+#' package = "parcutils")
+#' oo <- parcutils::get_intergenic_singals(bw_file = bw_file,gff_file  = gff_file)
+#' oo %>%
+#'     rtracklayer::export.bed(con = file(paste(bw_file_name,"_intergenic", ".bed",sep = "")))
+#' oo %>%
+#'    rtracklayer::export.bedGraph(con = file(paste(bw_file_name,"_intergenic", ".bdg",sep = "")))
+#' @importFrom rtracklayer import
+#' @importFrom tibble as_tibble
+#' @importFrom dplyr filter mutate
+#' @importFrom GenomicRanges GRanges mcolAsRleList binnedAverage gaps
+#' @importFrom tools file_path_sans_ext
+#' @importFrom magrittr %>%
 get_intergenic_singals <- function(bw_file , gff_file){
-
 
   gff_dat <- rtracklayer::import(gff_file)
 
   gff_genes <- gff_dat %>%
     tibble::as_tibble() %>%
-    dplyr::filter(type == "gene") %>%
+    dplyr::filter(.data$type == "gene") %>%
     GenomicRanges::GRanges()
 
   gff_intergenic <- gff_genes %>%
     tibble::as_tibble() %>%
     dplyr::mutate(strand = "+") %>%
     GenomicRanges::GRanges() %>%
-    gaps()
+    GenomicRanges::gaps()
 
   bw_dat <- rtracklayer::import(bw_file)
 
@@ -35,18 +49,6 @@ get_intergenic_singals <- function(bw_file , gff_file){
 
 }
 
-bw_files <- list.files("/Users/chiragparsania/Documents/Projects/10_Nandan/iMac_backup/Cg_Pol_II/2_fastq_and_alignment/xbp1_deletion/180518_D00691_0103_BCCHBVANXX/mapping/" ,
-                       pattern = "*.bw" , recursive = T ,full.names = T)
-
-gff_file <- "/Users/chiragparsania/Documents/Projects/10_Nandan/iMac_backup/Cg_Pol_II/1_genome/CBS138_s02-m07-r06/annotation/C_glabrata_CBS138_version_s02-m07-r06_features.gff"
-
-bw_file <- bw_files[1]
-bw_file_name <-  tools::file_path_sans_ext(bw_file)
-
-oo <- get_intergenic_singals(bw_file = bw_file,gff_file  = gff_file)
-
-oo %>% export.bed(con = file(paste(bw_file_name,"_intergenic", ".bed",sep = "")))
-oo %>% export.bedGraph(con = file(paste(bw_file_name,"_intergenic", ".bdg",sep = "")))
 
 
 
